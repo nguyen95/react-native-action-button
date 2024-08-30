ab9360c8a463eab820072e0a3bf74594f1ebf76d
pprazzoli@hes-eng.it <pprazzoli@hes-eng.it>
2024-08-30 11:54:02 +0200

    missing background image style

--- ActionButton.js
+++ ActionButton.js
@@ -442,6 +442,11 @@ const styles = StyleSheet.create({
     marginTop: -4,
     fontSize: 24,
     backgroundColor: "transparent"
-  }
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
 });
 export default ActionButton;
